23a09c430a081f2f1c1427712518fe0eb97fa1a4
Artem Raykh <77747763+jscelle@users.noreply.github.com>
2023-10-01 20:31:54 +0300

    Initial Commit

---
+++ animue/animueApp.swift
@@ -0,0 +1,17 @@
+//
+//  animueApp.swift
+//  animue
+//
+//  Created by Artem Raykh on 01.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct animueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
